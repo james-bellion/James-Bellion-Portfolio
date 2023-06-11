aedd2d8dc0defc57e08ba395eb6d19bc056d20ac
james-bellion <belliondjames@hotmail.co.uk>
2023-06-12 00:07:29 +1200

    added floating balls finished experiance section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
+// get our balls!!
+import { BallCanvas } from "./canvas" 
+
+import { SectionWrapper } from "../hoc"
+
+import { technologies } from "../constants"
+
+// loop over all of technologies saying tech.map get each induvidual tech and the for each
+// induvidual tech we return a div 
+// add self closing BallCanvas with icon as its only paramater
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div
+     className="flex flex-row flex-wrap justify-center gap-10">
+     {technologies.map((technology) => (
+       <div className="w-28 h-28" key={technology.name}>
+        <BallCanvas  icon={technology.icon}/>
+      </div>
+     ))}
+    
+
+    </div>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech, "")
